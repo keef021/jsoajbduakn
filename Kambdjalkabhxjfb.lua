07b89c04c7c8e66ce0cfd7106c468a5b285f26c7
keef021 <alexandremilas2011@gmail.com>
2025-08-12 00:23:00 -0300

    Update Kambdjalkabhxjfb.lua

--- Kambdjalkabhxjfb.lua
+++ Kambdjalkabhxjfb.lua
@@ -8,7 +8,7 @@ local Window = Libary:MakeWindow({
     Flags = "ChaosHub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://120052712529941", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://128161889268721", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -9241,4 +9241,5 @@ UniversalTab:AddButton({
         loadstring(game:HttpGet("https://raw.githubusercontent.com/SAZXHUB/Control-update/main/README.md", true))()
     end
 })
+
 
